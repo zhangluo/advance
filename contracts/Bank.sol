65228dff13bc34cea0e25b0686c2dec5907be236
zhangluo <luo3.zhang@tcl.com>
2024-07-08 10:16:23 +0800

    add homework

---
+++ contracts/Bank.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Bank {
+    // 状态变量
+    address public immutable owner;
+    // 事件
+    event Deposit(address _ads, uint256 amount);
+    event Withdraw(uint256 amount);
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+    constructor() payable {
+        owner = msg.sender;
+    }
+    function withdraw() external {
+        require(msg.sender == owner, "Not Owner");
+        emit Withdraw(address(this).balance);
+        selfdestruct(payable(msg.sender));
+    }
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
